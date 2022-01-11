f254aa11f398859eff4fc760010ff69e8f2cef9d
Matt <matt@jackalmedia.co.uk>
2022-01-11 17:58:55 +0000

    added server side api call to twitter, front end connected to app api, content being served via mapped component

---
+++ framestore-feed/pages/api/twitter.js
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async function handler(req, res) {
+  const token = 'Bearer AAAAAAAAAAAAAAAAAAAAAE77XwEAAAAAknHORpglJbMQcHnRoFmTQ7xdr5E%3Di6hUdyJJgco9jLcRdbBmlzU073105CtQ5cuAjVlJZrYRzrwAHR'
+  const url = 'https://api.twitter.com/2/users/33475281/tweets'
+
+  // make sure our API wont hang
+  if (req.method === 'GET') {
+    // fetch data from twitter api
+    const response = await fetch(url, {
+      headers: {
+        'Authorization': token,
+        }
+      })
+  
+      // await the response and return it
+      const data = await response.json()
+      return res.status(200).json(data)
+  } 
+  // exit if not GET
+  return res.status(405).json({msg: "error, unsupported http method"})
+}
