c5a5b7cf10cd0af3ece2fbff50425d863e83e477
Matt <matt@jackalmedia.co.uk>
2022-01-11 19:34:30 +0000

    wrap app in body to keep React/children error happy

--- framestore-feed/pages/_app.js
+++ framestore-feed/pages/_app.js
@@ -2,7 +2,11 @@ import '../styles/globals.css'
 import 'bulma/css/bulma.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <body className='column'>
+    <Component {...pageProps} />
+  </body>
+  )
 }
 
 export default MyApp
